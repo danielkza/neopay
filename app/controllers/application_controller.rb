b4d02eccdf7a7d4415e4f971b0587a5b4b6fdea1
Daniel Miranda <danielkza2@gmail.com>
2015-12-05 22:53:41 -0800

    fix json check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
 
   def json_only
-    render :nothing => true, :status => 406 unless params[:format] == 'json'
+    return if params[:format] == "json" || request.headers["Accept"] =~ /json/
+    render :nothing => true, :status => 406
   end
 end
