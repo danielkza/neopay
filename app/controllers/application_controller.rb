8010b98f5bc047ff7f43e577e54b13f83e01c892
Daniel Miranda <danielkza2@gmail.com>
2015-12-05 22:49:46 -0800

    rem. csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 
   def json_only
     render :nothing => true, :status => 406 unless params[:format] == 'json'
