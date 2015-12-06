b5f51e9f6ef86ad8b348aefa5a0cef801e25504d
Daniel Miranda <danielkza2@gmail.com>
2015-12-05 22:46:17 -0800

    user ssn param

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -87,6 +87,6 @@ class UsersController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
-    params.require(:user).permit(:name, :gender, :location, :birthday, :phone)
+    params.require(:user).permit(:name, :phone, :ssn)
   end
 end
